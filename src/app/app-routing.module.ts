7eba862e0d34ffc0410bcfc1554b65115c367a3b
Brayan Carabali <brayanlc82@gmail.com>
2020-12-18 13:27:18 -0500

    I corrected the navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContentLayoutComponent } from './modules/character/content-layout/content-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./modules/character/character.module').then(m => m.CharacterModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
